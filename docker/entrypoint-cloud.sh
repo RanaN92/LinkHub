e0cddf44ec20eb985652eef97494ad76251e5312
RanaN92 <159700038+RanaN92@users.noreply.github.com>
2024-03-07 21:24:40 +0200

    Update entrypoint-cloud.sh

--- docker/entrypoint-cloud.sh
+++ docker/entrypoint-cloud.sh
@@ -1,28 +1,13 @@
-# syntax=docker/dockerfile:1
-FROM node:18-alpine
+#!/bin/sh
 
-ENV PORT 3000
+set -e
 
-RUN \
-  apk --no-cache add --virtual build-dependencies python3 build-base git
+cd packages/backend
 
-RUN git clone https://github.com/RanaN92/LinkHub.git
-
-WORKDIR /LinkHub 
-# RUN yarn install
-# RUN cd /LinkHub
-# Copy package.json and yarn.lock
-# COPY package.json yarn.lock ./
-
-RUN yarn install
-
-RUN if [ "$WORKER" != "true" ]; then cd packages/web && yarn build; fi
-
-RUN \
-  rm -rf /usr/local/share/.cache/ && \
-  apk del build-dependencies
-
-COPY docker/entrypoint-cloud.sh /entrypoint-cloud.sh
-
-EXPOSE 3000
-ENTRYPOINT ["sh", "/entrypoint-cloud.sh"]
+if [ -n "$WORKER" ]; then
+  yarn start:worker
+else
+  yarn db:migrate
+  yarn db:seed:user
+  yarn start
+fi
